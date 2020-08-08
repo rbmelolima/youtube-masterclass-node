c7366458776ece72adf5d3e1f81a0fc0929eb64e
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-08 20:25:17 -0300

    feat: criação do servidor

---
+++ http/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+  const file = req.url === '/' ? 'index.html' : req.url;
+  const filePath = path.join(__dirname, 'public', file);
+  const extname = path.extname(filePath);
+
+  const allowedFilesTypes = ['.html', '.css', '.js'];
+  const allowed = allowedFilesTypes.find(item => item === extname);
+
+  if(!allowed) return;
+
+  fs.readFile(
+    filePath,
+    (error, content) => {
+      if(error) { throw error; }
+
+      res.end(content);
+    }
+  );
+
+}).listen(3333, () => console.log("server is running"));
+
